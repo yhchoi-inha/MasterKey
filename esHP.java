854f002f59337584f6a61b75dd0e5a8c7761d338
yhchoi-inha <yohan0312@inha.edu>
2023-04-11 21:57:37 +0900

    문제를 맞출 경우 각 문제에 해당하는 랜덤 숫자 생성/TER06

--- esHP.java
+++ esHP.java
@@ -16,6 +16,26 @@ public class esHP {
     static void q_01(){
         // 첫번째 문제의 답이 선언한 답과 같다면 +1(중복 방지)
         if(QU_01.answer.equals(es_qu1)){
+            // 최종 조합을 위한 랜덤 숫자 생성
+            a1=esRandomNum.esrNum();
+            esRoom.sleep(1000);
+            System.out.println();
+            System.out.println("정답!!");
+            esRoom.sleep(1000);
+            System.out.println();
+            System.out.println();
+            System.out.println("*****************");
+            // 랜덤 숫자 1개 받아줌
+            System.out.println("*\t"+a1+"\t*");
+            System.out.println("*****************");
+            esRoom.sleep(1000);
+            System.out.println();
+            System.out.println();
+            System.out.println("최종 문제를 위한 숫자 하나 획득");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            esRoom.sleep(1000);
             cnt++;
             QU_01.count1++;
         } else {
