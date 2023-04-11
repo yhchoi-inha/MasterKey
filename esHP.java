6ea04d7e3bb494c21ce2432d7bb822d22ae976f4
yhchoi-inha <yohan0312@inha.edu>
2023-04-11 21:34:55 +0900

    문제의 답안 및 정답,오답을 나타낼 구조 생성/TER03

---
+++ esHP.java
@@ -0,0 +1,31 @@
+public class esHP {
+    protected static int heart=50; // 게임 기회 세팅(임의 값 50)
+
+    static String es_qu1 = "1번 문제의 답";
+    static String es_qu2 = "2번 문제의 답";
+    static String es_qu3 = "3번 문제의 답";
+    static String es_qu4 = "4번 문제의 답";
+
+    static int cnt = 0; // ++
+    static int a1;
+    static int a2;
+    static int a3;
+    static int a4;
+
+    static void question_01(){
+
+    }
+
+    static void question_02(){
+
+    }
+
+    static void question_03(){
+
+    }
+
+    static void question_04(){
+
+    }
+
+}
