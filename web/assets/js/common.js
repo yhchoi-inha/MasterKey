a5afff90e52558dd6d02c58f6356eca9f60e1169
yhchoi-inha <yohan0312@inha.edu>
2023-05-05 16:35:08 +0900

    학교 테마 구분 레이어 스크립트 추가/WEB12

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -23,5 +23,36 @@ $(function(){
 		$('.main__notice-btn')[0].click();
 	});
 
+
+	// Mail Layer
+	$('.school__mail').on('click', function() {
+		showLayer('.layer-common--mail', '.layer-dimmed--mail', this);
+	});
+	$('.layer-common--mail .layer-common__close, .layer-dimmed--mail').on('click', function() {
+		$('.school__mail')[0].click();
+	});
+
+	// School Layer
+	$('.school__ignore').on('click', function() {
+		showLayer('.layer-common--ignore', '.layer-dimmed--ignore', this);
+	});
+	$('.layer-common--ignore .layer-common__close, .layer-dimmed--ignore').on('click', function() {
+		$('.school__ignore')[0].click();
+	});
+
+	function showLayer(layerClass, dimmedClass, trigger) {
+		if ($(trigger).hasClass('active') == false) {
+			$(dimmedClass).fadeIn(300, function() {
+				$(layerClass).fadeIn(300).css('display', 'flex');
+			});
+			$(trigger).addClass('active');
+		} else {
+			$(layerClass).fadeOut(300, function() {
+				$(dimmedClass).fadeOut(300);
+			});
+			$(trigger).removeClass('active');
+		}
+	}
+
 	// ing
 });
